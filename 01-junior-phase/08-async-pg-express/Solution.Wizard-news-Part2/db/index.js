d5f528e9ab511b98e4f3ddc45e46987f201f1082
David Patlut <dpatlut@gmail.com>
2019-07-27 14:49:14 -0400

    removed solution code and other stuffs

--- 01-junior-phase/08-async-pg-express/Solution.Wizard-news-Part2/db/index.js
+++
@@ -1,6 +0,0 @@
-const pg = require('pg');
-const client = new pg.Client('postgres://localhost/wnews');
-
-client.connect();
-
-module.exports = client;
--- 01-junior-phase/09-wikistack/Solution.Wikistack/views/index.js
+++
@@ -1,9 +0,0 @@
-const addPage = require("./addpage");
-const editPage = require("./editpage");
-const error = require("./error");
-const main = require("./main");
-const userList = require("./userlist");
-const userPages = require("./userpages");
-const wikiPage = require("./wikipage");
-
-module.exports = { addPage, editPage, error, main, userList, userPages, wikiPage };
